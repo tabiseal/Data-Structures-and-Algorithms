a482889ac0f46aa11253d7725a4ecff06a94be82
tabiseal <115387977+tabiseal@users.noreply.github.com>
2023-12-06 00:10:10 +0900

    Add files via upload

---
+++ 1.1/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//------------------
+//漸化式(nCrの計算)
+//------------------
+
+long combi(int n, int r)
+{
+	int i = 0;
+	long p = 1;
+	for (i = 1; i <= r; i++)
+	{
+		p = p * (n - i + 1) / i;
+	}
+	return p;
+}
+
+int main()
+{
+	int n = 0;
+	int r = 0;
+	for (n = 0; n <= 5; n++)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			printf("%dC%d=%ld  ", n, r, combi(n, r));
+		}
+		printf("\n");
+	}
+}
